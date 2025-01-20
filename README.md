a73e4e64ee4b71119b54b72b9aabb53e6e590d61
Felipe Macedo <felipe.macedo@capitanigroup.com>
2025-01-20 20:50:50 -0300

    POC making questions and understanding problem

---
+++ README.md
@@ -0,0 +1,10 @@
+## Chalenge
+ - Build a code generator from YAML file.
+
+## Flow
+-  Read file
+-  Interpreter the file
+
+
+### Doubts?
+- What is the file structure?
---
+++ src/main/java/br/com/xmacedo/CodeGeneratorWithYaml.java
@@ -0,0 +1,11 @@
+package br.com.xmacedo;
+
+public class CodeGeneratorWithYaml {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // Read file
+        // Interpreter the file
+        //  - What is the file structure?
+    }
+}
